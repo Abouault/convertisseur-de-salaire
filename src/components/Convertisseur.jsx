c20670d824a7a9ed491330d2224f014efff6793b
Aymeric <aymeric.bouault@gmail.com>
2023-06-11 19:35:13 +0200

    new repo

---
+++ src/components/Convertisseur.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SalaireMensuel from './SalaireMensuel';
+import SalaireAnnuel from './SalaireAnnuel';
+
+const Convertisseur = () => {
+    return (
+        <div>
+            <SalaireMensuel />
+            <SalaireAnnuel />
+        </div>
+    );
+};
+
+export default Convertisseur;
